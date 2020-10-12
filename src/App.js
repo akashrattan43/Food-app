2debc8235d0360c86c3c9915739b44b3e279fd89
akash rattan <akashrattan43@Gmail.com>
2020-10-12 14:17:57 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import Axios from 'axios'
+import Recipe from './components/Recipe'
+import {v4 as uuidv4} from 'uuid'
+import Alert from './components/Alert'
 
 function App() {
+ 
+  const[query, setQuery] = useState('')
+  const[recipes, setRecipes] = useState([])
+  const[alert, setAlert] = useState('')
+  const APP_ID = 'f3c34d43';
+
+  const APP_KEY = '113476a859772812f6a8302c0c72d077	'
+  
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const getData = async() => {
+    if(query !== '') {
+      const result = await Axios.get(url);
+      if(!result.data.more){
+        return setAlert('No Food With Such Name')
+      }
+      setRecipes(result.data.hits)
+      console.log(result)
+      setAlert('')
+      setQuery('')
+    } else {
+      setAlert ('Please Fill the Form')
+    }
+    
+  }
+
+  const onChange = e => {
+    setQuery(e.target.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Recipe Searcher</h1>
+      <form className='search-form' onSubmit={onSubmit}>
+      {alert !== '' && <Alert alert={alert}/>}
+        <input type='text' placeholder='Search Food' autoComplete='off' onChange={onChange} value={query}/>
+        <input type='submit' value='search' />
+      </form>
+      <div className='recipes'>
+        {recipes !==[] && recipes.map(recipe => 
+        <Recipe key={uuidv4()} recipe={recipe} />)}
+      </div>
     </div>
   );
 }
